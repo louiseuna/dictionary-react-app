f42f1192087b5d47e2592c86148f8fd59ec3543e
louiseuna <louiseuna15@gmail.com>
2021-10-10 12:34:42 +0100

    corrected phonetics so it works now!

--- src/Results.js
+++ src/Results.js
@@ -1,12 +1,20 @@
 import React from "react";
 import "./Dictionary.css";
 import Meaning from "./Meaning.js";
+import Phonetics from "./Phonetics.js";
 
 export default function Result(props) {
   if (props.results) {
     return (
       <div className="results">
         <h2>{props.results.word}</h2>
+        {props.results.phonetics.map(function (phonetic, index) {
+          return (
+            <div key={index}>
+              <Phonetics phonetic={phonetic} />
+            </div>
+          );
+        })}
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
